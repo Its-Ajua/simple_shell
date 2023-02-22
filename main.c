19feb09321a97f22d724431967b6c1f782f36bf7
Its-Ajua <akinladerebecca04@gmail.com>
2023-02-22 14:50:29 -0800

    Shell

--- main.c
+++ main.c
@@ -1,44 +1,49 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * main - Checks for a command prompt and executes
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success
  */
-int main(int ac, char **av)
+
+int main(void)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	char *command = NULL, **tkn = NULL;
+	int length = 0, flag = 0;
+	size_t line_size = 0;
+	ssize_t line_len = 0;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-
-	if (ac == 2)
+	while (line_len >= 0)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		signal(SIGINT, sign_handler);
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "($) ", 4);
+		line_len = getline(&command, &line_size, stdin);
+		if (line_len == -1)
 		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
 		}
-		info->readfd = fd;
+
+		length = word_count(command);
+		if (command[0] != '\n' && length > 0)
+		{
+			tkn = tokenize(command, " \t", length);
+			flag = execbuilt(tkn, command);
+			if (!flag)
+			{
+				tkn[0] = find(tkn[0]);
+				if (tkn[0] && access(tkn[0], X_OK) == 0)
+					exec(tkn[0], tkn);
+				else
+					perror("./hsh");
+			}
+
+			frees_tkn(tkn);
+		}
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+
+	free(command);
+	return (0);
 }
